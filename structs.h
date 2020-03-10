6195bf543ae10765e2e7adf75e705f27269075ab
FTheodore <theofil97@gmail.com>
2020-03-10 19:04:40 +0200

    initial commit

---
+++ structs.h
@@ -0,0 +1,14 @@
+//
+// Created by theo on 10/3/20.
+//
+
+#ifndef SYSPRO_1_STRUCTS_H
+#define SYSPRO_1_STRUCTS_H
+
+typedef struct StringxPtr{
+    void * ptr;
+    char * string;
+}StringxPtr;
+
+
+#endif //SYSPRO_1_STRUCTS_H
